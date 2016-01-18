ad625e8b8e2e6f5df7adae6f310b4ac7c3f07d3b
Nate Clark <nathan.clark@ignitionone.com>
2016-01-18 16:47:04 -0500

    Teaking display

--- src/server/middleware/crossOrigin.js
+++ src/server/middleware/crossOrigin.js
@@ -3,8 +3,7 @@ const cors = require('express-cors');
 module.exports = function(app) {
   app.use(cors({
     allowedOrigins: [
-      '*',
-      'https://www.twinspires.com'
+      '*'
     ]
   }));
 };
