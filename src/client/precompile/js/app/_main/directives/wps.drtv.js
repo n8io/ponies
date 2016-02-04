1c588ca989c1fc5dcf25dc6ae0c3266514614291
Nate Clark <nathan.clark@ignitionone.com>
2016-02-03 22:29:07 -0500

    Removed non functioning code

--- src/client/precompile/js/app/_main/directives/wps.drtv.js
+++ src/client/precompile/js/app/_main/directives/wps.drtv.js
@@ -15,26 +15,13 @@
       replace: true,
       restrict: 'E',
       template: `
-        <div class='wps-container' data-ng-show='!!wps' data-ng-click='showPayouts(wps)'>
+        <div class='wps-container' data-ng-show='!!wps'>
           <div class='wps-wrapper'>
             <span data-ng-bind='wps | wpsSelections'>
             </span>
           </div>
         </div>
-      `,
-      controller: controllerFn
+      `
     };
-
-    function controllerFn() {
-      const vm = this; // eslint-disable-line
-
-      vm.showPayouts = showPayouts;
-
-      function showPayouts(wps) {
-        $mdBottomSheet.show({
-          template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
-        });
-      }
-    }
   }
 })();
